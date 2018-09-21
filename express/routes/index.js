fab976ea22677302eaaa36fb3b984b04a2aaf115
xmcl1 <1009818761@qq.com>
2018-09-22 03:25:59 +0800

    完成居家页面全部数据模拟，及数据图片

--- express/routes/index.js
+++ express/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var shiwu=require('../data/shiwu')
 var jujia=require('../data/jujia')
+var WYYX=require('../data/WYYX')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -19,6 +20,12 @@ router.get("/jujia",function(req,res,next){
   res.header("Access-Control-Allow-Headers", "X-Requested-With,Origin,Content-Type,Accept");
   res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
   res.json(jujia);
+});
+router.get("/WYYX",function(req,res,next){
+  res.header("Access-Control-Allow-Origin","*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With,Origin,Content-Type,Accept");
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+  res.json(WYYX);
 });
 
 
