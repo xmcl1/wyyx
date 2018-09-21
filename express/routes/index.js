cbaa847a5b62dc5762462080c4e8cc678eab70f8
xmcl1 <1009818761@qq.com>
2018-09-21 14:03:07 +0800

    模拟数据 居家等

--- express/routes/index.js
+++ express/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var shiwu=require('../data/shiwu')
+var jujia=require('../data/jujia')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,6 +13,12 @@ router.get("/shiwu",function(req,res,next){
   res.header("Access-Control-Allow-Headers", "X-Requested-With,Origin,Content-Type,Accept");
   res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
   res.json(shiwu);
+});
+router.get("/jujia",function(req,res,next){
+  res.header("Access-Control-Allow-Origin","*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With,Origin,Content-Type,Accept");
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+  res.json(jujia);
 });
 
 
