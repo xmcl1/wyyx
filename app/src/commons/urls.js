4139969c372d038697b5f02b318a68944fe64fbc
xmcl1 <1009818761@qq.com>
2018-09-23 22:39:45 +0800

    修改（宠物）数据

--- app/src/commons/urls.js
+++ app/src/commons/urls.js
@@ -1,3 +1,4 @@
 export const LIMITTIMEAPI = 'http://localhost:3000/api/limittime'
 export const NEWPROAPI = 'http://localhost:3000/api/newpro'
 export const JUJIA = 'http://localhost:3000/api/juJia'
+
--- express/routes/index.js
+++ express/routes/index.js
@@ -8,22 +8,27 @@ var limitTime = require("../data/limitTime");
 var newPro = require("../data/newPro");
 var juJia = require("../data/juJia");
 
+// 解决跨域
+router.all('*',function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+
+    if (req.method == 'OPTIONS') {
+        res.send(200); /让options请求快速返回/
+    }
+    else {
+        next();
+    }
+});
+
 router.get("/api/juJia", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With,Origin,Content-Type,Accept");
-  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
   res.json(juJia)
 }),
 router.get("/api/limitTime", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With,Origin,Content-Type,Accept");
-  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
   res.json(limitTime)
 }),
 router.get("/api/newPro", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With,Origin,Content-Type,Accept");
-  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
   res.json(newPro)
 })
 
@@ -35,22 +40,33 @@ router.get('/', function (req, res, next) {
 });
 
 router.get("/shiWu", function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With,Origin,Content-Type,Accept");
-  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
   res.json(shiWu);
 });
 router.get("/juJia", function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With,Origin,Content-Type,Accept");
-  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
   res.json(juJia);
 });
 router.get("/WYYX", function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With,Origin,Content-Type,Accept");
-  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
   res.json(WYYX);
+});
+// 商品详情
+router.get("/api/productgoods", function (req, res, next) {
+    let tempQuery = req.query;
+    let productgoods = { products:null };
+    // console.log(tempQuery);
+    for(let jj in juJia){
+        if(juJia[jj].menuname==tempQuery.fenleimenu){
+          for(let iss in juJia[jj].allSort){
+              if((juJia[jj].allSort[iss].id)==tempQuery.xiaofenlei){
+                for(let isss in juJia[jj].allSort[iss].products){
+                    if(juJia[jj].allSort[iss].products[isss].index==tempQuery.shangpinId){
+                        productgoods.products = juJia[jj].allSort[iss].products[isss];
+                    }
+                }
+              }
+          }
+        }
+    }
+    res.json(productgoods);
 });
 
 
